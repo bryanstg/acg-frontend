4b08f2ece30a8e4dfe7f9406e9ce0bf7d0d23ef6
Bryan García <bryan1997st@gmail.com>
2021-11-24 18:50:55 -0500

    Create and styled navbar

---
+++ src/js/component/Navbar.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export const Navbar = () => {
+	return (
+		<nav className="navbar-market">
+			<h2>Market Place</h2>
+		</nav>
+	);
+};
