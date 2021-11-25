482f9a7cce416eb3f7c75d05c65998df5b063179
Bryan García <bryan1997st@gmail.com>
2021-11-25 01:51:53 -0500

    🔨Tabs created and styled. Needs logic to work properly

---
+++ src/js/component/Tabs.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const Tabs = () => {
+	return (
+		<div className="tabs">
+			<div className="tab">{`Pestaña ${1}`}</div>
+			<div className="tab">{`Pestaña ${2}`}</div>
+			<div className="tab">{`Pestaña ${3}`}</div>
+			<div className="tab">{`Pestaña ${4}`}</div>
+		</div>
+	);
+};
